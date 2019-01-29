0117b7ba4182471972edf12d1ba0d9f9f7c06d78
zhoujin <zhoujin@iboxpay.com>
2019-01-29 15:19:32 +0800

    update

---
+++ Puppy.java
@@ -0,0 +1,8 @@
+public class Puppy{
+  public Puppy(String name){
+    System.out.println("小狗的名字是:"+ name);
+  }
+  public static void main(String []args){
+    Puppy myPuppy = new Puppy("tommy");
+  }
+}
