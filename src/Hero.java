b16d69aeea6855299a93e0ad3532ce7b2b7fb025
1391020381@qq.com <1391020381@qq.com>
2019-04-15 21:13:37 +0800

    熟悉一些方法

--- src/Hero.java
+++ src/Hero.java
@@ -1,14 +1,6 @@
-public class Hero {
+class Hero {
     String name;
     float hp;
     float armor ;
     int moveSpeed;
-    public static void main (String[] args){
-        Hero garen = new Hero();
-        garen.name = "盖伦";
-        garen.hp = 616.28f;
-        garen.armor = 27.536f;
-        garen.moveSpeed = 350;
-        System.out.println(garen);
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+            Hero garen = new Hero();
+            garen.name = "盖伦";
+            garen.hp = 616.28f;
+            garen.armor = 27.536f;
+            garen.moveSpeed = 350;
+            System.out.println(garen);
+            System.out.println("Hello World!");
+            int size = 10;
+            double[] myList = new double[size];
+            myList[0] = 5.6;
+            myList[1] = 4.5;
+            double total = 0;
+            for(int i =0;i<size;i++){
+                total += myList[i];
+            }
+            System.out.println("总和为："+ total);
+
     }
 }
