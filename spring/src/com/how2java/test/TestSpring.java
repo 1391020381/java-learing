d1f56b4ebb089e611f400345242ca3536e092968
1391020381@qq.com <1391020381@qq.com>
2019-05-12 11:23:47 +0800

    第一个spring项目

---
+++ spring/src/com/how2java/test/TestSpring.java
@@ -0,0 +1,14 @@
+package com.how2java.test;
+
+
+import com.how2java.pojo.Category;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String [] args){
+        ApplicationContext context =  new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
+        Category c = (Category) context.getBean("c");
+        System.out.println(c.getName());
+    }
+}
