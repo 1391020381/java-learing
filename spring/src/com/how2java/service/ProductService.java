33967c9b29eb38da6b46ba40862e934dc682f787
1391020381@qq.com <1391020381@qq.com>
2019-05-13 20:58:02 +0800

    注释切面aop

---
+++ spring/src/com/how2java/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.how2java.service;
+
+import org.springframework.stereotype.Component;
+
+@Component("s")
+public class ProductService {
+    public void doSomeService(){
+        System.out.println("doSomeService");
+    }
+}
--- spring/src/com/how2java/test/TestSpring.java
+++ spring/src/com/how2java/test/TestSpring.java
@@ -3,6 +3,7 @@ package com.how2java.test;
 
 import com.how2java.pojo.Category;
 import com.how2java.pojo.Product;
+import com.how2java.service.ProductService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,6 +15,7 @@ public class TestSpring {
         Product p = (Product) context.getBean("p");
         System.out.println(p.getName());
         System.out.println(p.getCategory());
-
+        ProductService s = (ProductService) context.getBean("s");
+        s.doSomeService();
     }
 }
