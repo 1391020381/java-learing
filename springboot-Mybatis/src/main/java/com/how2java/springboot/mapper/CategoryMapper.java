3f176a8de0e1ac8c23406e118b8908f828611faf
zhoujin <zhoujin@iboxpay.com>
2019-05-17 14:35:27 +0800

    springboot+mybatis简单增删改查

--- springboot-Mybatis/src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
+++ springboot-Mybatis/src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
@@ -2,9 +2,11 @@ package com.how2java.springboot.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
+import org.apache.ibatis.annotations.Update;
 import com.how2java.springboot.pojo.Category;
 
 @Mapper
@@ -12,6 +14,18 @@ public interface CategoryMapper {
 
     @Select("select * from category_ ")
     List<Category> findAll();
+
+    @Insert(" insert into category_ ( name ) values (#{name}) ")
+    public int save(Category category);
+
+    @Delete(" delete from category_ where id= #{id} ")
+    public void delete(int id);
+
+    @Select("select * from category_ where id= #{id} ")
+    public Category get(int id);
+
+    @Update("update category_ set name=#{name} where id=#{id} ")
+    public int update(Category category);
 
 
 }
