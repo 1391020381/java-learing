9c7052a7b3bda9add607b157e2d4922b051db12b
p_jinzjzhou <p_jinzjzhou@tencent.com>
2022-11-21 18:47:05 +0800

    eclipse java Practices

---
+++ Java核心类.md
@@ -0,0 +1,5 @@
+# String
+
+- 两个字符串比较 必须总是使用 equals()方法
+
+* 要忽略大小写比较 使用 equalsIgnoreCase() 方法
---
+++ hello/src/Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * Learn Java from https://www.liaoxuefeng.com/
+ * 
+ * @author liaoxuefeng
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+
+}
